3cd60deea768f6132c249080475004109c4fa005
knie20 <kdnie20@gmail.com>
2024-06-21 00:44:41 -0400

    add https stuff

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+//cert pass: ligmaballs
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
+
+// Load the SSL certificates
+const privateKey = fs.readFileSync(path.join(__dirname, 'key.pem'), 'utf8')
+const certificate = fs.readFileSync(path.join(__dirname, 'cert.pem'), 'utf8')
+const credentials = { key: privateKey, cert: certificate }
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('KDNIE WORKS!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+  console.log(`HTTPS Server listening on port ${port}`)
 })
